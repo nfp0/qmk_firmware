d676dccd740f892a031c45f8bed1f7a99a62df05
Nuno Pereira <nf.pereira@outlook.pt>
2019-09-12 20:21:40 +0100

    macros

--- keyboards/solheim68/keymaps/default/keymap.c
+++ keyboards/solheim68/keymaps/default/keymap.c
@@ -20,21 +20,56 @@
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 
+uint16_t shifts_held = 0;
+
+enum custom_keycodes {
+  M_LBRC = SAFE_RANGE,
+  M_RBRC,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-    KC_ESC,      KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_HOME, KC_PGUP, \
-    KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, XXXXXXX,          KC_END,  KC_PGDN, \
-    KC_CAPS,     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,                             \
-    KC_LSFT,     KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, XXXXXXX,          KC_UP,            \
-    KC_LCTL,     KC_LGUI, KC_LALT,                            KC_SPC,                             MO(1),   KC_RGUI, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT  \
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,          KC_HOME, KC_PGUP, \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, XXXXXXX,          KC_END,  KC_PGDN, \
+    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_NUHS, KC_ENT,                             \
+    KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, XXXXXXX,          KC_UP,            \
+    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             MO(1),   KC_RGUI,  KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
 
   [1] = LAYOUT(
-    LALT(KC_F4), KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,           KC_INS,  KC_MPRV, \
-    _______,     _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_DEL,  KC_MNXT, \
-    _______,     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______, _______, _______,                            \
-    _______,     KC_GRV,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_VOLU,          \
-    _______,     _______, _______,                            _______,                            _______, _______, _______,          KC_MUTE, KC_VOLD, KC_MPLY  \
+    A(KC_F4), KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,           KC_INS,  KC_MPRV, \
+    _______,  _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, M_LBRC,  M_RBRC,  _______,          KC_DEL,  KC_MNXT, \
+    _______,  _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______, _______, _______,                            \
+    _______,  KC_GRV,  _______, _______, _______, _______, _______, _______, _______, _______, _______, S(KC_7), _______, _______,          KC_VOLU,          \
+    _______,  _______, _______,                            _______,                            _______, _______, _______,          KC_MUTE, KC_VOLD, KC_MPLY  \
   ),
 
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+	switch(keycode) {
+    case KC_LSFT:
+    case KC_RSFT:
+      if(record->event.pressed)
+        shifts_held++;
+      else {
+        shifts_held--;
+      }
+      break;
+    case M_LBRC:
+      if (record->event.pressed) {
+        if(shift_held) {
+          unregister_code(KC_LSFT);
+					unregister_code(KC_RSFT);
+					register_code(RALT(KC_7));
+        }
+        else {
+          register_code(RALT(KC_8));
+        }
+        
+  }
+	return true;
+};
+
+
+
