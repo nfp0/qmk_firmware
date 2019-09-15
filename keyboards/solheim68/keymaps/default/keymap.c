da02e52ddee348077596ac344fb95e4d5b8c7d60
Nuno Pereira <nf.pereira@outlook.pt>
2019-09-15 21:43:03 +0100

    Removed AltGr layer and changed menu key

--- keyboards/solheim68/keymaps/default/keymap.c
+++ keyboards/solheim68/keymaps/default/keymap.c
@@ -26,9 +26,7 @@
 #define RBRC RALT(KC_9)
 #define LCBR RALT(KC_7)
 #define RCBR RALT(KC_0)
-#define MENUWIN RGUI_T(KC_APP)
-#define NUMCAPS LT(3, KC_CAPS)
-#define L_ALTGR LM(2, MOD_LCTL | MOD_LALT)
+#define NUMCAPS LT(2, KC_CAPS)
 
 enum custom_keycodes {
   M_LBRC = SAFE_RANGE,
@@ -49,22 +47,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, LBRC,    RBRC,    _______,          KC_DEL,  KC_VOLD, \
     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, AT,      LCBR,    RCBR,    _______,                            \
     _______, KC_GRV,  _______, _______, _______, _______, _______, _______, _______, _______, _______, S(KC_7), _______, _______,          KC_MPLY,          \
-    _______, _______, _______,                            _______,                            _______, MENUWIN, L_ALTGR,          KC_MPRV, KC_MUTE, KC_MNXT  \
+    _______, _______, _______,                            KC_APP,                             _______, _______, _______,          KC_MPRV, KC_MUTE, KC_MNXT  \
   ),
 
   [2] = LAYOUT(
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_HOME, KC_PGUP, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, XXXXXXX,          KC_END,  KC_PGDN, \
-    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,                             \
-    KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, XXXXXXX,          KC_UP,            \
-    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             _______, KC_RGUI, _______,          KC_LEFT, KC_DOWN, KC_RGHT  \
-    ),
-
-  [3] = LAYOUT(
     _______, _______, _______, _______, _______, _______, _______, _______, KC_PSLS, KC_PAST, KC_PMNS, _______, _______, _______,          KC_NLCK, _______, \
     _______, _______, _______, _______, _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, _______, _______, _______,          _______, _______, \
     _______, _______, _______, _______, _______, _______, _______, KC_P4,   KC_P5,   KC_P6,   _______, _______, _______, KC_PENT,                            \
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   _______, _______, _______,          _______,          \
+    _______, _______, _______, _______, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   _______, _______, _______, _______,          _______,          \
     _______, _______, _______,                            KC_P0,                              KC_PDOT, _______, _______,          _______, _______, _______  \
   ),
 };
