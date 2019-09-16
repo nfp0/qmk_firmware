cd90f024eac5cd6055cfafc7beccc920b615624c
Nuno Pereira <nf.pereira@outlook.pt>
2019-09-16 21:31:29 +0100

    Fn switch and Numlock transparencies

--- keyboards/solheim68/keymaps/default/keymap.c
+++ keyboards/solheim68/keymaps/default/keymap.c
@@ -22,6 +22,7 @@
 
 #define ALT_F4 LALT(KC_F4)
 #define AT RALT(KC_2)
+#define EURO RALT(KC_E)
 #define LBRC RALT(KC_8)
 #define RBRC RALT(KC_9)
 #define LCBR RALT(KC_7)
@@ -39,22 +40,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, XXXXXXX,          KC_END,  KC_PGDN, \
     NUMCAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,                             \
     KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, XXXXXXX,          KC_UP,            \
-    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RGUI, MO(1),   KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT  \
+    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             MO(1),   KC_RGUI, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
 
   [1] = LAYOUT(
     ALT_F4,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,           KC_INS,  KC_VOLU, \
-    _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, LBRC,    RBRC,    _______,          KC_DEL,  KC_VOLD, \
+    _______, _______, KC_UP,   EURO,    _______, _______, _______, _______, _______, _______, _______, LBRC,    RBRC,    _______,          KC_DEL,  KC_VOLD, \
     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, AT,      LCBR,    RCBR,    _______,                            \
     _______, KC_GRV,  _______, _______, _______, _______, _______, _______, _______, _______, _______, S(KC_7), _______, _______,          KC_MPLY,          \
-    _______, _______, _______,                            _______,                            _______, _______, KC_APP,           KC_MPRV, KC_MUTE, KC_MNXT  \
+    _______, _______, _______,                            _______,                            _______, KC_APP,  _______,          KC_MPRV, KC_MUTE, KC_MNXT  \
   ),
 
   [2] = LAYOUT(
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSLS, KC_PAST, KC_PMNS, XXXXXXX, XXXXXXX, _______,          KC_NLCK, XXXXXXX, \
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, \
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P4,   KC_P5,   KC_P6,   XXXXXXX, XXXXXXX, XXXXXXX, KC_PENT,                            \
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P1,   KC_P2,   KC_P3,   XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,          \
-    XXXXXXX, XXXXXXX, XXXXXXX,                            KC_P0,                              KC_PDOT, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX  \
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P1,   KC_P2,   KC_P3,   XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,          \
+    _______, _______, _______,                            KC_P0,                              KC_PDOT, _______, _______,          XXXXXXX, XXXXXXX, XXXXXXX  \
   ),
 };
