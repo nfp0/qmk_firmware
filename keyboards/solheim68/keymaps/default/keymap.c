0d2f46150dba6f7cf08c64b1fb782b30b0b23f47
Nuno Pereira <nf.pereira@outlook.pt>
2019-12-21 19:54:15 +0000

    new navigation and numlock

--- keyboards/solheim68/keymaps/default/keymap.c
+++ keyboards/solheim68/keymaps/default/keymap.c
@@ -40,16 +40,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [1] = LAYOUT(
     ALT_F4,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,           KC_INS,  KC_VOLU, \
-    _______, AT,      KC_UP,   EURO,    _______, _______, _______, _______, _______, _______, _______, LBRC,    RBRC,    _______,          KC_DEL,  KC_VOLD, \
-    TG(2),   KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, KC_HOME, KC_PGUP, LCBR,    RCBR,    _______,                            \
-    _______, KC_GRV,  _______, _______, _______, _______, _______, _______, _______, _______, KC_END,  KC_PGDN, _______, _______,          KC_MPLY,          \
+    _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, _______, _______, KC_INS,  _______, _______, LBRC,    RBRC,    _______,          KC_DEL,  KC_VOLD, \
+    TG(2),   KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, LCBR,    RCBR,    _______,                            \
+    _______, KC_GRV,  _______, _______, _______, _______, _______, _______, _______, _______, _______, S(KC_7), _______, _______,          KC_MPLY,          \
     _______, _______, _______,                            KC_APP,                             _______, _______, _______,          KC_MPRV, KC_MUTE, KC_MNXT  \
   ),
 
   [2] = LAYOUT(
-    KC_NLCK, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSLS, KC_PAST, KC_PMNS, XXXXXXX, XXXXXXX, _______,          _______, _______, \
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSLS, KC_PAST, KC_PMNS, XXXXXXX, XXXXXXX, _______,          _______, _______, \
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, XXXXXXX, XXXXXXX, XXXXXXX,          _______, _______, \
-    TG(2),   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P4,   KC_P5,   KC_P6,   XXXXXXX, XXXXXXX, XXXXXXX, KC_PENT,                            \
+    TG(2),   XXXXXXX, XXXXXXX, XXXXXXX, KC_NLCK, XXXXXXX, XXXXXXX, KC_P4,   KC_P5,   KC_P6,   XXXXXXX, XXXXXXX, XXXXXXX, KC_PENT,                            \
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P1,   KC_P2,   KC_P3,   XXXXXXX, _______, XXXXXXX,          _______,          \
     _______, _______, _______,                            KC_P0,                              KC_PDOT, _______, _______,          _______, _______, _______  \
   ),
