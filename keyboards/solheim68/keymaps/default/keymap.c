33872e35451e33320ad393975be53c5e6a97c92c
Nuno Pereira <nf.pereira@outlook.pt>
2020-01-19 15:56:43 +0000

    shifted arrows right

--- keyboards/solheim68/keymaps/default/keymap.c
+++ keyboards/solheim68/keymaps/default/keymap.c
@@ -37,9 +37,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [1] = LAYOUT(
     ALT_F4,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,           KC_INS,  KC_VOLU, \
-    _______, XXXXXXX, KC_UP,   XXXXXXX, KC_PGUP, XXXXXXX, XXXXXXX, XXXXXXX, KC_INS,  XXXXXXX, XXXXXXX, LBRC,    RBRC,    XXXXXXX,          KC_DEL,  KC_VOLD, \
-    TG(2),   KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, XXXXXXX, KC_PSCR, KC_SLCK, KC_PAUS, KC_HOME, KC_PGUP, LCBR,    RCBR,    _______,                            \
-    _______, KC_GRV,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, S(KC_7), KC_END,  KC_PGDN, _______, XXXXXXX,          KC_MPLY,          \
+    _______, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_INS,  XXXXXXX, KC_HOME, KC_UP,   KC_END,  XXXXXXX,          KC_DEL,  KC_VOLD, \
+    TG(2),   KC_LEFT, KC_DOWN, KC_RGHT, KC_PSCR, KC_SLCK, KC_PAUS, XXXXXXX, XXXXXXX, KC_PGUP, KC_LEFT, KC_DOWN, KC_RGHT, _______,                            \
+    _______, KC_GRV,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGDN, S(KC_7), _______, XXXXXXX,          KC_MPLY,          \
     _______, _______, _______,                            KC_APP,                             _______, _______, _______,          KC_MPRV, KC_MUTE, KC_MNXT  \
   ),
 
