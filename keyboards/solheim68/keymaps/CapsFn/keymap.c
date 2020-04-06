4289a18d83a5702ac93204b8f3d4b9f371874b29
Nuno Pereira <nf.pereira@outlook.pt>
2020-04-06 14:17:11 +0100

    removed RAlt modifier

--- keyboards/solheim68/keymaps/CapsFn/keymap.c
+++ keyboards/solheim68/keymaps/CapsFn/keymap.c
@@ -21,7 +21,7 @@
 // entirely and just use numbers.
 
 #define ALT_F4 LALT(KC_F4)
-#define MO_DOT LT(2, KC_PDOT)
+#define MO_DOT LALT_T(KC_PDOT)
 #define LBRC RALT(KC_8)
 #define RBRC RALT(KC_9)
 #define LCBR RALT(KC_7)
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, XXXXXXX,          KC_END,  KC_PGDN, \
     MO(2),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,                             \
     KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, XXXXXXX,          KC_UP,            \
-    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             MO(2),   KC_RGUI, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT  \
+    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_LALT, KC_RGUI, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
 
   [1] = LAYOUT(
